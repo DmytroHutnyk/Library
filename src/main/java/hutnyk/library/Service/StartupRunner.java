6164b59179b36441199935f162fb7057f78660d7
Dmytro Hutnyk <s28785@pjwstk.edu.pl>
2025-02-01 13:12:27 +0100

    feat: added application exit option

--- src/main/java/hutnyk/library/Service/StartupRunner.java
+++ src/main/java/hutnyk/library/Service/StartupRunner.java
@@ -1,10 +1,17 @@
 package hutnyk.library.Service;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
+import java.util.Scanner;
+
+@RequiredArgsConstructor
 @Component
 public class StartupRunner implements CommandLineRunner {
+    private final ApplicationContext applicationContext;
 
     @Override
     public void run(String... args) {
@@ -14,5 +21,24 @@ public class StartupRunner implements CommandLineRunner {
         System.out.println(" Librarian: Username: user3 | Password: pjatk");
         System.out.println(" Admin:     Username: admin | Password: asd3");
         System.out.println("\n Application available at  http://localhost:8080\n");
+
+        listenForExitCommand();
+    }
+
+    private void listenForExitCommand() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter 'q' to exit the application.\n");
+
+        while (true) {
+            String input = scanner.nextLine().trim();
+
+            if ("q".equalsIgnoreCase(input)) {
+                System.out.println("Shutting down...");
+                SpringApplication.exit(applicationContext, () -> 0);
+                break;
+            }
+        }
+
+        scanner.close();
     }
 }
